033a63ded37bafb4ec14791129cd4e2f2f14285b
JoverZhang <joverzh@gmail.com>
2020-06-19 23:41:21 +0800

    feat(install.sh): mount bin to /home/jover, use unionfs.

--- install.sh
+++ install.sh
@@ -10,8 +10,19 @@ JOVER_VERSION='0.0.1-test.1'
 
 git clone https://github.com/JoverZhang/mylinux.git $HOME/.mylinux
 
-mylinux_env="export JOVER=$HOME/.mylinux"
+# Inject to user startup cycle.
+mylinux_env="export JOVER=$HOME/.mylinux" # create env for project location.
+bin_path="export PATH=$PATH:$HOME/bin" # add $HOME/bin to $PATH
+mount_bin="mount -t unionfs -o dirs=$JOVER/bin unionfs $HOME/bin" # use unionfs mount bin
+
+mkdir -p $HOME/bin
 
 sed -i "s|$mylinux_env||" $HOME/.bashrc
 sed -i "$ a $mylinux_env" $HOME/.bashrc
+
+sed -i "s|$bin_path||" $HOME/.bashrc
+sed -i "$ a $bin_path" $HOME/.bashrc
+
+sed -i "s|$mount_bin||" $HOME/.bashrc
+sed -i "$ a $mount_bin" $HOME/.bashrc
 
